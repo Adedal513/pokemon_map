680f1bec183a9090ac467c29a6e4a1945978f2b4
Nikolay Moroz <adedalmoroz@gmail.com>
2022-06-06 07:00:23 +0300

    add relation one-to-one for pokemon models

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -16,6 +16,23 @@ DEFAULT_IMAGE_URL = (
 )
 
 
+def get_evolution_serialized(pokemon: Pokemon, request):
+    if pokemon is None:
+        return None
+
+    pokemon_image_uri = None
+
+    if pokemon.image:
+        pokemon_image_uri = request.build_absolute_uri(pokemon.image.url)
+
+    pokemon_serialized = {
+        "title_ru": pokemon.title,
+        "pokemon_id": pokemon.id,
+        "img_url": pokemon_image_uri
+    }
+
+    return pokemon_serialized
+
 
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
     icon = folium.features.CustomIcon(
@@ -74,27 +91,31 @@ def show_pokemon(request, pokemon_id):
     except exceptions.ObjectDoesNotExist:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
+    requested_pokemon_image_uri = None
+
+    if requested_pokemon.image:
+        requested_pokemon_image_uri = request.build_absolute_uri(requested_pokemon.image.url)
+
     requested_pokemon_serialized = {
         "pokemon_id": pokemon_id,
         "title_ru": requested_pokemon.title,
         "title_en": requested_pokemon.title_en,
         "title_jp": requested_pokemon.title_jp,
         "description": requested_pokemon.description,
-        "img_url": requested_pokemon.image.url
+        "img_url": requested_pokemon_image_uri,
+        "next_evolution": get_evolution_serialized(requested_pokemon.evolves_to, request),
+        "previous_evolution": get_evolution_serialized(requested_pokemon.evolves_from, request)
     }
 
     pokemon_entities = PokemonEntity.objects.filter(pokemon_id=int(pokemon_id))
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
-    requested_pokemon_image_uri = request.build_absolute_uri(requested_pokemon.image.url)
-
-    if requested_pokemon_image_uri:
-        for pokemon_entity in pokemon_entities:
-            add_pokemon(
-                folium_map, pokemon_entity.lat,
-                pokemon_entity.lon,
-                requested_pokemon_image_uri
-            )
+    for pokemon_entity in pokemon_entities:
+        add_pokemon(
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.lon,
+            requested_pokemon_image_uri
+        )
 
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(), 'pokemon': requested_pokemon_serialized
