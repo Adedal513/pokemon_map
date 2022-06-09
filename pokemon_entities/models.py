6971371a2e2db2244a6aa3284aaa266d5851e68b
Nikolay Moroz <adedalmoroz@gmail.com>
2022-06-09 21:13:45 +0300

    added verbose names to all models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,14 +4,22 @@ import django.utils.timezone as timezone
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
     image = models.ImageField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    evolves_from = models.ForeignKey(to='self', on_delete=models.RESTRICT, related_name='previous_evolutions', null=True, blank=True, default=None)
+    description = models.TextField(default='Об этом покемоне нам пока ничего не известно :(', blank=True)
+    evolves_from = models.ForeignKey(to='self',
+                                     on_delete=models.RESTRICT,
+                                     related_name='previous_evolutions',
+                                     null=True,
+                                     blank=True,
+                                     default=None)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'pokemon'
 
 
 class PokemonEntity(models.Model):
@@ -22,10 +30,10 @@ class PokemonEntity(models.Model):
     disappeared_at = models.DateTimeField(default=None)
 
     level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    health = models.IntegerField(default=0, blank=True, null=True)
+    strength = models.IntegerField(default=0, blank=True, null=True)
+    defence = models.IntegerField(default=0, blank=True, null=True)
+    stamina = models.IntegerField(default=0, blank=True, null=True)
 
     def is_available(self):
         appeared_at_local = timezone.localtime(self.appeared_at)
@@ -38,3 +46,6 @@ class PokemonEntity(models.Model):
 
     def __str__(self):
         return f'{self.pokemon.title} {self.level} lvl.'
+
+    class Meta:
+        verbose_name = 'pokemon entity'
